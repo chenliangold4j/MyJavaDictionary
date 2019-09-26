fb998fdce38df2c3d23feba97e2cfcd22e670b50
phantom <chenliangxingji1@163.com>
2019-09-26 17:55:28 +0800

    init

---
+++ src/main/java/self/liang/collection/example/ArrayListExample.java
@@ -0,0 +1,11 @@
+package self.liang.collection.example;
+
+import java.util.ArrayList;
+
+public class ArrayListExample
+{
+   public static void main(String[] args)
+   {
+      ArrayList<Integer> list ;
+   }
+}
