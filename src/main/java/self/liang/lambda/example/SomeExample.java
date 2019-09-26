d81dc697ce164faef0b98cdb1546d99a22fba502
phantom <chenliangxingji1@163.com>
2019-09-26 18:04:40 +0800

    init

--- src/main/java/self/liang/lambda/example/SomeExample.java
+++ src/main/java/self/liang/lambda/example/SomeExample.java
@@ -9,6 +9,22 @@ public class SomeExample
    
    public static void main(String[] args)
    {
+      
+   // Java 8之前：
+      List<String> features1 = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+      for (String feature : features1) {
+          System.out.println(feature);
+      }
+
+      // Java 8之后：
+      List<String> features2 = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+      features2.forEach(n -> System.out.println(n));
+       
+      // 使用Java 8的方法引用更方便，方法引用由::双冒号操作符标示，
+      // 看起来像C++的作用域解析运算符
+      features2.forEach(System.out::println);
+      
+      
       List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
       
       System.out.println("Languages which starts with J :");
@@ -35,4 +51,6 @@ public class SomeExample
           }
       }
   }
+   
+   
 }
