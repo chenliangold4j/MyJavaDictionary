1650ddedf4e83c4d7a847988f129fd608d4f4f61
chenliang <chenliangxingji1@163.com>
2019-10-20 22:12:28 +0800

    10 20

--- src/main/java/self/liang/springmvc/example/controller/bindparams/BindParamsController.java
+++ src/main/java/self/liang/springmvc/example/controller/bindparams/BindParamsController.java
@@ -38,6 +38,11 @@ public class BindParamsController {
     @ResponseBody
     @RequestMapping("/testPojo")
     public Car testPojo(UserBind userBind){
+//        Content-Type: application/x-www-form-urlencoded
+//        username=%E5%9C%B0%E6%96%B9&account=test2323&password=213&age=11&address.city=px&address.province=province&cars%5B0%5D.name=wuling&cars%5B0%5D.price=50000&cars%5B1%5D.name=benchi&cars%5B1%5D.price=5000000&carMap%5B%22one%22%5D.name=baoma&carMap%5B%22one%22%5D.price=78945612
+//        carMap["one"].name = baoma 传map
+//        cars[1].name = "baoma"传list
+//        address.city=px  传bean
         System.out.println(userBind);
         System.out.println(car);
         return car;
@@ -54,6 +59,9 @@ public class BindParamsController {
     @ResponseBody
     @RequestMapping("/bindDate")
     public String bindDate(Date date){
+//        Content-Type: application/x-www-form-urlencoded
+//
+//        date=2019-10-11
         System.out.println(date);
         return date.toString();
     }
