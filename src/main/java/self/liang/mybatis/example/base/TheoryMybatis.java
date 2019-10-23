2d9a6aea541ae0079690ae3f17fd0ae233196767
chenliang <chenliangxingji1@163.com>
2019-10-24 00:21:15 +0800

    start mybatis in

---
+++ src/main/java/self/liang/mybatis/example/base/TheoryMybatis.java
@@ -0,0 +1,18 @@
+package self.liang.mybatis.example.base;
+
+/** 一
+ * 最上层是接口层：
+ *
+ *  其次是数据处理层；
+ *      参数映射（ParameterHandler）--->sql解析（slqSource）---->sql执行（executor）--->结果处理和映射(resultSetHandler）
+ *
+ *  框架支撑层：
+ *          xml，注解，事务管理，连接池管理，缓存机制
+ *
+ *   引导层：xml配置，java api
+ *
+ */
+
+public class TheoryMybatis {
+
+}
