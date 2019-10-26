8eed8ca8dd794d06b35c4d9658c5bb0db7fce2b1
phantom <chenliangxingji1@163.com>
2019-10-26 16:51:11 +0800

    aaa

---
+++ src/main/java/self/liang/dynamic/proxy/example/IUserManager.java
@@ -0,0 +1,7 @@
+package self.liang.dynamic.proxy.example;
+
+public interface IUserManager {
+
+    public void addUser(Integer id,String name);
+    public void delUser(Integer id);
+}
---
+++ src/main/java/self/liang/dynamic/proxy/example/cglib/CGLibTest2.java
@@ -0,0 +1,13 @@
+package self.liang.dynamic.proxy.example.cglib;
+
+public class CGLibTest2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void test1(){
+
+    }
+
+}
