31c214b27982a1abfa19feaf5991f260cd30dc47
phantom <chenliangxingji1@163.com>
2019-10-26 15:17:40 +0800

    fix

--- src/main/java/self/liang/concurrent/example/example/atomic/AtomicExample5.java
+++ src/main/java/self/liang/concurrent/example/example/atomic/AtomicExample5.java
@@ -13,20 +13,24 @@ public class AtomicExample5 {
     private static AtomicIntegerFieldUpdater<AtomicExample5> updater =
             AtomicIntegerFieldUpdater.newUpdater(AtomicExample5.class, "count");
 
-    public static volatile int count = 100;
+    public volatile int count = 100;
 
     public static void main(String[] args) {
 
         AtomicExample5 example5 = new AtomicExample5();
 
         if (updater.compareAndSet(example5, 100, 120)) {
-            log.info("update success 1, {}"+count);
+            log.info("update success 1, {}"+example5.count);
         }
 
         if (updater.compareAndSet(example5, 100, 120)) {
-            log.info("update success 2, {}"+count);
+            log.info("update success 2, {}"+example5.count);
         } else {
-            log.info("update failed, {}"+count);
+            log.info("update failed, {}"+example5.count);
         }
+    }
+
+    public int getCount() {
+        return count;
     }
 }
