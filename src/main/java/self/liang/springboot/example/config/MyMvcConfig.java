c4f5f432011336ea51958ce1c65692978feb9dc1
phantom <chenliangxingji1@163.com>
2019-11-26 17:32:16 +0800

    keep

--- src/main/java/self/liang/springboot/example/config/MyMvcConfig.java
+++ src/main/java/self/liang/springboot/example/config/MyMvcConfig.java
@@ -1,9 +1,12 @@
 package self.liang.springboot.example.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import self.liang.springmvc.example.MyFirstInterceptor;
+
+import java.util.List;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
@@ -11,5 +14,11 @@ public class MyMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
 //        registry.addInterceptor(new MyFirstInterceptor());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+//        resolvers.add(new TestXmlMethodArumentResolver());
+
     }
 }
