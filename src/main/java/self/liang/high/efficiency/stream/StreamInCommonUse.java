fc7454e7d15c5ac38a7b4f620ac8cacf77adcb5b
chenliang <chenliangxingji1@163.com>
2020-08-08 16:38:05 +0800

    fix

--- src/main/java/self/liang/high/efficiency/stream/StreamInCommonUse.java
+++ src/main/java/self/liang/high/efficiency/stream/StreamInCommonUse.java
@@ -11,15 +11,26 @@ public class StreamInCommonUse {
 //            +--------------------+       +------+   +------+   +---+   +-------+
 
     public static void main(String[] args) {
+
+        List<String> strings = Arrays.asList("abc", "", "bc", "efg", "abcd", "", "jkl");
+// 获取空字符串的数量
+        long count = strings.parallelStream().filter(string -> string.isEmpty()).count();
+        System.out.println(count);
+        String collect = strings.stream().collect(Collectors.joining(","));
+        System.out.println(collect);
+
+    }
+
+    public void test1() {
         List<String> list = new ArrayList<>();
         Random random = new Random();
-        random.ints().limit(100).forEach(num->{
+        random.ints().limit(100).forEach(num -> {
             System.out.println(num);
             list.add(Integer.toString(num));
         });
 
-        List<String> arrs =  list.stream().filter(str->{
-            return  Integer.parseInt(str) > 0;
+        List<String> arrs = list.stream().filter(str -> {
+            return Integer.parseInt(str) > 0;
         }).collect(Collectors.toList());
 
         System.out.println(Arrays.toString(arrs.toArray()));
