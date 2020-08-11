cea0a6914ff7568730edabe5b633cabac6f8465e
chenliang <chenliangxingji1@163.com>
2020-08-11 11:27:51 +0800

    fix

--- src/main/java/self/liang/velocity/VelocityTest1.java
+++ src/main/java/self/liang/velocity/VelocityTest1.java
@@ -1,4 +1,41 @@
 package self.liang.velocity;
 
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VelocityTest1 {
+
+    private static final String VM_PATH = "template/velocity/helloworld.vm";
+
+    public static void main(String[] args) {
+        // 初始化模板引擎
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+        velocityEngine.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+        velocityEngine.init();
+
+        // 获取模板文件
+        Template template = velocityEngine.getTemplate(VM_PATH);
+
+        // 设置变量，velocityContext是一个类似map的结构
+        VelocityContext velocityContext = new VelocityContext();
+        velocityContext.put("name", "world");
+        List<String> list = new ArrayList<String>();
+        list.add("jack");
+        list.add("kitty");
+        velocityContext.put("list", list);
+
+        // 输出渲染后的结果
+        StringWriter stringWriter = new StringWriter();
+        template.merge(velocityContext, stringWriter);
+        System.out.println(stringWriter.toString());
+    }
+
 }
