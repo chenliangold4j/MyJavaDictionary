2317792f5a1f0909fff406eef189f644f864c15d
chenliang <chenliangxingji1@163.com>
2020-08-28 17:03:29 +0800

    fix

--- src/main/java/self/liang/concurrent/example/example/threadLocal/TransmittableThreadLocalTest1.java
+++ src/main/java/self/liang/concurrent/example/example/threadLocal/TransmittableThreadLocalTest1.java
@@ -2,6 +2,21 @@ package self.liang.concurrent.example.example.threadLocal;
 
 public class TransmittableThreadLocalTest1 {
 
+    public static void main(String[] args) {
+        TransmittableThreadLocal<String> context = new TransmittableThreadLocal<String>();
 
+        context.set("value-set-in-parent")
+        println("[parent thread] set ${context.get()}")
+
+        /////////////////////////////////////
+        // create sub-thread
+        /////////////////////////////////////
+        thread {
+            val value = context.get()
+            println("[child thread] get $value")
+        }.join();
+
+        println("[parent thread] get ${context.get()}")
+    }
 
 }
