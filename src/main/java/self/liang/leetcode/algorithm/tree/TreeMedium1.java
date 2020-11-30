290334c1d746dc560d488ce0091425c143d51d7f
chenliang <chenliangxingji1@163.com>
2020-11-30 22:30:03 +0800

    keep

--- src/main/java/self/liang/leetcode/algorithm/tree/TreeMedium1.java
+++ src/main/java/self/liang/leetcode/algorithm/tree/TreeMedium1.java
@@ -4,43 +4,42 @@ import java.util.Stack;
 
 /**
  * 给定一个二叉树，原地将它展开为链表。
- *
+ * <p>
  * 例如，给定二叉树
- *
- *     1
- *    / \
- *   2   5
- *  / \   \
+ * <p>
+ * 1
+ * / \
+ * 2   5
+ * / \   \
  * 3   4   6
  * 将其展开为：
- *
+ * <p>
  * 1
- *  \
- *   2
- *    \
- *     3
- *      \
- *       4
- *        \
- *         5
- *          \
- *           6
- *
+ * \
+ * 2
+ * \
+ * 3
+ * \
+ * 4
+ * \
+ * 5
+ * \
+ * 6
  */
 public class TreeMedium1 {
 
 
     /**
      * 问题是怎么想到这个思路：
-     *      还是看了答案：
-     *          应该是从叶子节点开始。
-     *          将某个节点的左子树和右子树都转为 只有右节点的子树
-     *          然后在将他们拼接成链表。
+     * 还是看了答案：
+     * 应该是从叶子节点开始。
+     * 将某个节点的左子树和右子树都转为 只有右节点的子树
+     * 然后在将他们拼接成链表。
      */
     public void flatten(TreeNode root) {
 
         //核心逻辑是从叶子开始转链表
-        if(root == null)return; //到了空节点返回。之后处理的都是有左右节点的节点。
+        if (root == null) return; //到了空节点返回。之后处理的都是有左右节点的节点。
         flatten(root.left);
         flatten(root.right);
 
@@ -58,53 +57,52 @@ public class TreeMedium1 {
         //虽然是链表了，但是和题目顺序不符合。提交的还是答案。但是不用顺序的的话，这个代码还少一行。
         TreeNode temporary = root.left;
         root.left = null;
-        while(root.right != null) root = root.right;
+        while (root.right != null) root = root.right;
         root.right = temporary;
     }
 
 
-    private void method1(TreeNode root){
-        if(root == null) return;
+    private void method1(TreeNode root) {
+        if (root == null) return;
         method1(root.left);
         method1(root.right);
         TreeNode tmp = root.right;
         root.right = root.left;
         root.left = null;
-        while(root.right != null) root = root.right;
+        while (root.right != null) root = root.right;
         root.right = tmp;
     }
 
     public void method2(TreeNode root) {
         Stack<TreeNode> stack = new Stack();
-        while (root != null || !stack.isEmpty()){
-            while (root != null){
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
                 stack.push(root);
                 root = root.left;
             }
 
-            if (!stack.isEmpty()){
+            if (!stack.isEmpty()) {
                 TreeNode node = stack.pop();
                 TreeNode tmp = node.right;
                 node.right = node.left;
                 node.left = null;
 
-                while(node.right != null) node = node.right;
+                while (node.right != null) node = node.right;
                 node.right = tmp;
                 root = tmp;
             }
         }
     }
 
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
 
-
-
-}
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int x) {
-        val = x;
+        TreeNode(int x) {
+            val = x;
+        }
     }
+
+
 }
